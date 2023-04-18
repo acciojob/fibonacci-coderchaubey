c56e4c58712f372a69713115e54f4dc52e54c161
coderchaubey <115737868+coderchaubey@users.noreply.github.com>
2023-04-18 12:24:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function fibonacci(num) {
 // your code here
+	if (num===0) {
+		return 0;
+	}else if (num===1) {
+		return 1;
+	}
+	return fibonacci(num-2)+fibonacci(num-1);
 }
 
 module.exports = fibonacci;
